a847082589ae4569f75e4eb21aa10af93f8b2494
M.Saleh <mega_unknown@hotmail.com>
2020-07-11 09:11:15 -0500

    Adding Home Nav and Home Page

--- onlineShopping/src/main/java/edu/miu/waa/onlineShopping/controller/HomeController.java
+++ onlineShopping/src/main/java/edu/miu/waa/onlineShopping/controller/HomeController.java
@@ -1,10 +1,13 @@
 package edu.miu.waa.onlineShopping.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class HomeController {
-    @RequestMapping("/")
-    public String goHome() {
+    @GetMapping("/")
+    public String greet(Model model) {
+        model.addAttribute("firstName", "Josh");
         return "home";
     }
 }
