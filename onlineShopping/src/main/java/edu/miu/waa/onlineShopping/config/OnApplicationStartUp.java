e3f6e988e84ab51cf6cae9532e5c1f42e3efaf51
omarAlbaarah <64056063+omarAlbaarah@users.noreply.github.com>
2020-07-14 14:55:59 -0500

    Fix Init Database

--- onlineShopping/src/main/java/edu/miu/waa/onlineShopping/config/OnApplicationStartUp.java
+++ onlineShopping/src/main/java/edu/miu/waa/onlineShopping/config/OnApplicationStartUp.java
@@ -49,20 +49,9 @@ public class OnApplicationStartUp {
 
 	@EventListener
 	public void onApplicationEvent(ContextRefreshedEvent event) throws Exception {
-		createAdminUser();
-
 		createFolder();
 
 		CreateInitDataBaseInfo();
-
-		createBuyer("buyer1",UserStatus.APPROVED);
-		createBuyer("buyer2",UserStatus.PENDING);
-		createBuyer("buyer3",UserStatus.APPROVED);
-
-//		createSeller("seller",UserStatus.APPROVED);
-/*		createSeller("seller3",UserStatus.APPROVED);
-		createSeller("seller2",UserStatus.PENDING);*/
-
 	}
 
 	private void createFolder() {
@@ -74,64 +63,19 @@ public class OnApplicationStartUp {
 
 	}
 
-	private void createAdminUser() throws ParseException, IOException {
-		Admin user = new Admin();
-		user.setFirstName("admin");
-		user.setLastName("admin");
-		user.setEmail("admin@miu.edu");
-		user.setPhoneNumber("6418192921");
-		user.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
-		user.setUsername("admin");
-		user.setPassword("admin");
-		user.setApproved(UserStatus.APPROVED);
-		user.setRole(Role.ADMIN);
-		adminService.saveUser(user);
-	}
-
-
-
-
-	private void createBuyer(String username, UserStatus userStatus) throws ParseException, IOException {
-		Buyer user = new Buyer();
-		user.setEmail("Mohamedsaleh1984@hotmail.com");
-		user.setPhoneNumber("6418192921");
-		user.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
-		user.setRole(Role.BUYER);
-		user.setShoppingCart(new ShoppingCart());
-		user.setFirstName(username);
-		user.setLastName(username);
-		user.setUsername(username);
-		user.setPassword(username);
-		user.setApproved(userStatus);
-		user.setRole(Role.BUYER);
-		user.setShoppingCart(new ShoppingCart());
-
-		CardPayment cardPayment = new CardPayment();
-		cardPayment.setCardNumber("1234567812345678");
-		cardPayment.setExpiryDate("09/24");
-		cardPayment.setCvv("123");
-		cardPayment.setNameOnCard(username);
-		user.setCardPayment(cardPayment);
-		ShippingAddress address = new ShippingAddress();
-		address.setCountry("usa");
-		address.setState("iowa");
-		address.setCity("fairfield");
-		address.setStreet("1000 north");
-		address.setZipCode("52557");
-		user.setShippingAddress(address);
-
-		BillingAddress billingAddress = new BillingAddress();
-		billingAddress.setCountry("usa");
-		billingAddress.setState("iowa");
-		billingAddress.setCity("fairfield");
-		billingAddress.setStreet("1000 north");
-		billingAddress.setZipCode("52557");
-		user.setBillingAddress(billingAddress);
-		buyerService.saveUser(user);
-
-	}
-
 	private void CreateInitDataBaseInfo() throws ParseException {
+		Admin adminUser = new Admin();
+		adminUser.setFirstName("admin");
+		adminUser.setLastName("admin");
+		adminUser.setEmail("admin@miu.edu");
+		adminUser.setPhoneNumber("6418192921");
+		adminUser.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
+		adminUser.setUsername("admin");
+		adminUser.setPassword("admin");
+		adminUser.setApproved(UserStatus.APPROVED);
+		adminUser.setRole(Role.ADMIN);
+		adminService.saveUser(adminUser);
+
 		/******************** Product Category ************************/
 		ProductCategory productCategory = new ProductCategory();
 		productCategory.setName("Clothing");
@@ -153,71 +97,77 @@ public class OnApplicationStartUp {
 		productCategory.setName("Smart Home");
 		productCategoryService.save(productCategory);
 		/******************** Seller ************************/
-		Seller user = new Seller();
-		user.setFirstName("Mohmed");
-		user.setLastName("Zezo");
-		user.setEmail("seller@miu.edu");
-		user.setPhoneNumber("6418192921");
-		user.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
-		user.setUsername("seller");
-		user.setPassword("seller");
-		user.setApproved(UserStatus.APPROVED);
-		user.setRole(Role.SELLER);
-		user.setProducts(new HashSet<>());
+		Seller sellerUser1 = new Seller();
+		sellerUser1.setFirstName("Mohmed");
+		sellerUser1.setLastName("Zezo");
+		sellerUser1.setEmail("seller@miu.edu");
+		sellerUser1.setPhoneNumber("6418192921");
+		sellerUser1.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
+		sellerUser1.setUsername("seller1");
+		sellerUser1.setPassword("seller1");
+		sellerUser1.setApproved(UserStatus.APPROVED);
+		sellerUser1.setRole(Role.SELLER);
+		sellerUser1.setProducts(new HashSet<>());
+	//	sellerService.saveUser(sellerUser1);
+	//	sellerUser1 = sellerService.getSellerByID(1L);
 		/***********************Add Associate Products **********************/
 		Product product = new Product();
 		product.setDescription("Shoes desc");
 		product.setName("shirt");
 		product.setPrice(new BigDecimal(100));
-		product.setSeller(user);
+		product.setSeller(sellerUser1);
 		product.setStockQuantity((long) 15);
 		product.setProductCategory(productCategoryService.getProductCategoryByName("Clothing"));
 		product.setProductNumber(1L);
+		//productService.save(product);
 
 		Product product2 = new Product();
 		product2.setDescription("Dell Laptop 142L");
 		product2.setName("Dell Laptop 142L");
 		product2.setPrice(new BigDecimal(800));
 		product2.setStockQuantity((long) 7);
-		product2.setSeller(user);
+		product2.setSeller(sellerUser1);
 		product2.setProductCategory(productCategoryService.getProductCategoryByName("Computers"));
 		product2.setProductNumber(2L);
+		//productService.save(product2);
 
 		Product product3 = new Product();
 		product3.setDescription("Men Shirt");
 		product3.setName("Men Shirt");
 		product3.setPrice(new BigDecimal(15));
 		product3.setStockQuantity((long) 10);
-		product3.setSeller(user);
+		product3.setSeller(sellerUser1);
 		product3.setProductCategory(productCategoryService.getProductCategoryByName("Clothing"));
 		product3.setProductNumber(3L);
+		//productService.save(product3);
 
 		Product product4 = new Product();
 		product4.setDescription("C++ for Beginners");
 		product4.setName("C++");
 		product4.setPrice(new BigDecimal(100));
 		product4.setStockQuantity((long) 17);
-		product4.setSeller(user);
+		product4.setSeller(sellerUser1);
 		product4.setProductCategory(productCategoryService.getProductCategoryByName("Books"));
 		product4.setProductNumber(4L);
+		//productService.save(product4);
 
 
-		user.getProducts().add(product);
-		user.getProducts().add(product2);
-		user.getProducts().add(product3);
-		user.getProducts().add(product4);
+		sellerUser1.getProducts().add(product);
+		sellerUser1.getProducts().add(product2);
+		sellerUser1.getProducts().add(product3);
+		sellerUser1.getProducts().add(product4);
+		sellerService.saveUser(sellerUser1);
 
-		sellerService.saveUser(user);
 		/**************************************************************************************************/
 		/*********************************** Seller 2 *****************************************************/
 		Seller sellerAccount = new Seller();
 		sellerAccount.setFirstName("Mohmed");
-		sellerAccount.setLastName("Zezo");
+		sellerAccount.setLastName("Abdulla");
 		sellerAccount.setEmail("seller@miu.edu");
 		sellerAccount.setPhoneNumber("6418192921");
 		sellerAccount.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
-		sellerAccount.setUsername("seller");
-		sellerAccount.setPassword("seller");
+		sellerAccount.setUsername("seller2");
+		sellerAccount.setPassword("seller2");
 		sellerAccount.setApproved(UserStatus.APPROVED);
 		sellerAccount.setRole(Role.SELLER);
 		sellerAccount.setProducts(new HashSet<>());
@@ -240,12 +190,47 @@ public class OnApplicationStartUp {
 		titanic.setProductCategory(productCategoryService.getProductCategoryByName("Movies,Music"));
 		titanic.setProductNumber(6L);
 
-
 		sellerAccount.getProducts().add(amrCD);
 		sellerAccount.getProducts().add(titanic);
-
-
 		sellerService.saveUser(sellerAccount);
+		/**************************************************************************************************/
+		Buyer buyerUser = new Buyer();
+		buyerUser.setEmail("Mohamedsaleh1984@hotmail.com");
+		buyerUser.setPhoneNumber("6418192921");
+		buyerUser.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
+		buyerUser.setRole(Role.BUYER);
+		buyerUser.setShoppingCart(new ShoppingCart());
+		buyerUser.setFirstName("Mohamed");
+		buyerUser.setLastName("Kelany");
+		buyerUser.setUsername("buyer1");
+		buyerUser.setPassword("buyer1");
+		buyerUser.setApproved(UserStatus.APPROVED);
+		buyerUser.setRole(Role.BUYER);
+		buyerUser.setShoppingCart(new ShoppingCart());
+
+		CardPayment cardPayment = new CardPayment();
+		cardPayment.setCardNumber("1234567812345678");
+		cardPayment.setExpiryDate("09/24");
+		cardPayment.setCvv("123");
+		cardPayment.setNameOnCard("Card Holder");
+		buyerUser.setCardPayment(cardPayment);
+		ShippingAddress address = new ShippingAddress();
+		address.setCountry("usa");
+		address.setState("iowa");
+		address.setCity("fairfield");
+		address.setStreet("1000 north");
+		address.setZipCode("52557");
+		buyerUser.setShippingAddress(address);
+
+		BillingAddress billingAddress = new BillingAddress();
+		billingAddress.setCountry("usa");
+		billingAddress.setState("iowa");
+		billingAddress.setCity("fairfield");
+		billingAddress.setStreet("1000 north");
+		billingAddress.setZipCode("52557");
+		buyerUser.setBillingAddress(billingAddress);
+		buyerService.saveUser(buyerUser);
+
 
 	}
 }
