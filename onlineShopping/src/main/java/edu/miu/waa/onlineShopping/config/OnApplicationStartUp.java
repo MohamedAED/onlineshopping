2d6deb1459f530ff6645a6ee672a69f5359708b3
omarAlbaarah <64056063+omarAlbaarah@users.noreply.github.com>
2020-07-14 14:32:45 -0500

    Fix Initial Database

--- onlineShopping/src/main/java/edu/miu/waa/onlineShopping/config/OnApplicationStartUp.java
+++ onlineShopping/src/main/java/edu/miu/waa/onlineShopping/config/OnApplicationStartUp.java
@@ -52,12 +52,14 @@ public class OnApplicationStartUp {
 		createAdminUser();
 
 		createFolder();
-		createProductCategories();
+
+		CreateInitDataBaseInfo();
+
 		createBuyer("buyer1",UserStatus.APPROVED);
 		createBuyer("buyer2",UserStatus.PENDING);
 		createBuyer("buyer3",UserStatus.APPROVED);
 
-		createSeller("seller",UserStatus.APPROVED);
+//		createSeller("seller",UserStatus.APPROVED);
 /*		createSeller("seller3",UserStatus.APPROVED);
 		createSeller("seller2",UserStatus.PENDING);*/
 
@@ -86,88 +88,7 @@ public class OnApplicationStartUp {
 		adminService.saveUser(user);
 	}
 
-	public void createProductCategories(){
-		ProductCategory productCategory = new ProductCategory();
-		productCategory.setName("Clothing");
-		productCategoryService.save(productCategory);
 
-		productCategory = new ProductCategory();
-		productCategory.setName("Books");
-		productCategoryService.save(productCategory);
-
-		productCategory = new ProductCategory();
-		productCategory.setName("Movies,Music");
-		productCategoryService.save(productCategory);
-
-		productCategory = new ProductCategory();
-		productCategory.setName("Computers");
-		productCategoryService.save(productCategory);
-
-		productCategory = new ProductCategory();
-		productCategory.setName("Smart Home");
-		productCategoryService.save(productCategory);
-	}
-
-	private void createSeller(String username, UserStatus userStatus) throws ParseException, IOException {
-		Seller user = new Seller();
-		user.setFirstName(username);
-		user.setLastName(username);
-		user.setEmail("seller@miu.edu");
-		user.setPhoneNumber("6418192921");
-		user.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
-		user.setUsername(username);
-		user.setPassword(username);
-		user.setApproved(userStatus);
-		user.setRole(Role.SELLER);
-		user.setProducts(new HashSet<>());
-
-		Product product = new Product();
-		product.setDescription("Shoes desc");
-		product.setName("shirt");
-		product.setPrice(new BigDecimal(100));
-		product.setSeller(user);
-		product.setStockQuantity((long) 15);
-		product.setProductCategory(productCategoryService.getProductCategoryByName("Clothing"));
-		product.setProductNumber(1L);
-
-		Product product2 = new Product();
-		product2.setDescription("Dell Laptop 142L");
-		product2.setName("Dell Laptop 142L");
-		product2.setPrice(new BigDecimal(800));
-		product2.setStockQuantity((long) 7);
-		product2.setSeller(user);
-		product2.setProductCategory(productCategoryService.getProductCategoryByName("Computers"));
-		product2.setProductNumber(2L);
-
-
-
-		Product product3 = new Product();
-		product3.setDescription("Men Shirt");
-		product3.setName("Men Shirt");
-		product3.setPrice(new BigDecimal(15));
-		product3.setStockQuantity((long) 10);
-		product3.setSeller(user);
-		product3.setProductCategory(productCategoryService.getProductCategoryByName("Clothing"));
-		product3.setProductNumber(3L);
-
-
-		Product product4 = new Product();
-		product4.setDescription("C++ for Beginners");
-		product4.setName("C++");
-		product4.setPrice(new BigDecimal(100));
-		product4.setStockQuantity((long) 17);
-		product4.setSeller(user);
-		product4.setProductCategory(productCategoryService.getProductCategoryByName("Books"));
-		product4.setProductNumber(4L);
-
-
-		user.getProducts().add(product);
-		user.getProducts().add(product2);
-		user.getProducts().add(product3);
-		user.getProducts().add(product4);
-
-		sellerService.saveUser(user);
-	}
 
 
 	private void createBuyer(String username, UserStatus userStatus) throws ParseException, IOException {
@@ -207,6 +128,124 @@ public class OnApplicationStartUp {
 		billingAddress.setZipCode("52557");
 		user.setBillingAddress(billingAddress);
 		buyerService.saveUser(user);
+
+	}
+
+	private void CreateInitDataBaseInfo() throws ParseException {
+		/******************** Product Category ************************/
+		ProductCategory productCategory = new ProductCategory();
+		productCategory.setName("Clothing");
+		productCategoryService.save(productCategory);
+
+		productCategory = new ProductCategory();
+		productCategory.setName("Books");
+		productCategoryService.save(productCategory);
+
+		productCategory = new ProductCategory();
+		productCategory.setName("Movies,Music");
+		productCategoryService.save(productCategory);
+
+		productCategory = new ProductCategory();
+		productCategory.setName("Computers");
+		productCategoryService.save(productCategory);
+
+		productCategory = new ProductCategory();
+		productCategory.setName("Smart Home");
+		productCategoryService.save(productCategory);
+		/******************** Seller ************************/
+		Seller user = new Seller();
+		user.setFirstName("Mohmed");
+		user.setLastName("Zezo");
+		user.setEmail("seller@miu.edu");
+		user.setPhoneNumber("6418192921");
+		user.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
+		user.setUsername("seller");
+		user.setPassword("seller");
+		user.setApproved(UserStatus.APPROVED);
+		user.setRole(Role.SELLER);
+		user.setProducts(new HashSet<>());
+		/***********************Add Associate Products **********************/
+		Product product = new Product();
+		product.setDescription("Shoes desc");
+		product.setName("shirt");
+		product.setPrice(new BigDecimal(100));
+		product.setSeller(user);
+		product.setStockQuantity((long) 15);
+		product.setProductCategory(productCategoryService.getProductCategoryByName("Clothing"));
+		product.setProductNumber(1L);
+
+		Product product2 = new Product();
+		product2.setDescription("Dell Laptop 142L");
+		product2.setName("Dell Laptop 142L");
+		product2.setPrice(new BigDecimal(800));
+		product2.setStockQuantity((long) 7);
+		product2.setSeller(user);
+		product2.setProductCategory(productCategoryService.getProductCategoryByName("Computers"));
+		product2.setProductNumber(2L);
+
+		Product product3 = new Product();
+		product3.setDescription("Men Shirt");
+		product3.setName("Men Shirt");
+		product3.setPrice(new BigDecimal(15));
+		product3.setStockQuantity((long) 10);
+		product3.setSeller(user);
+		product3.setProductCategory(productCategoryService.getProductCategoryByName("Clothing"));
+		product3.setProductNumber(3L);
+
+		Product product4 = new Product();
+		product4.setDescription("C++ for Beginners");
+		product4.setName("C++");
+		product4.setPrice(new BigDecimal(100));
+		product4.setStockQuantity((long) 17);
+		product4.setSeller(user);
+		product4.setProductCategory(productCategoryService.getProductCategoryByName("Books"));
+		product4.setProductNumber(4L);
+
+
+		user.getProducts().add(product);
+		user.getProducts().add(product2);
+		user.getProducts().add(product3);
+		user.getProducts().add(product4);
+
+		sellerService.saveUser(user);
+		/**************************************************************************************************/
+		/*********************************** Seller 2 *****************************************************/
+		Seller sellerAccount = new Seller();
+		sellerAccount.setFirstName("Mohmed");
+		sellerAccount.setLastName("Zezo");
+		sellerAccount.setEmail("seller@miu.edu");
+		sellerAccount.setPhoneNumber("6418192921");
+		sellerAccount.setDateOfBirth(new SimpleDateFormat("MM/dd/yyyy").parse("03/22/1990"));
+		sellerAccount.setUsername("seller");
+		sellerAccount.setPassword("seller");
+		sellerAccount.setApproved(UserStatus.APPROVED);
+		sellerAccount.setRole(Role.SELLER);
+		sellerAccount.setProducts(new HashSet<>());
+		/***********************Add Associate Products **********************/
+		Product amrCD = new Product();
+		amrCD.setDescription("Amr Diab CD");
+		amrCD.setName("Amr Diab CD");
+		amrCD.setPrice(new BigDecimal(100));
+		amrCD.setSeller(sellerAccount);
+		amrCD.setStockQuantity((long) 15);
+		amrCD.setProductCategory(productCategoryService.getProductCategoryByName("Movies,Music"));
+		amrCD.setProductNumber(5L);
+
+		Product titanic = new Product();
+		titanic.setDescription("Dell Laptop 142L");
+		titanic.setName("Dell Laptop 142L");
+		titanic.setPrice(new BigDecimal(800));
+		titanic.setStockQuantity((long) 7);
+		titanic.setSeller(sellerAccount);
+		titanic.setProductCategory(productCategoryService.getProductCategoryByName("Movies,Music"));
+		titanic.setProductNumber(6L);
+
+
+		sellerAccount.getProducts().add(amrCD);
+		sellerAccount.getProducts().add(titanic);
+
+
+		sellerService.saveUser(sellerAccount);
 
 	}
 }
